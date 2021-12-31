069e469dc77cba5762f1ba08d60342eaa21ce4a6
Nam Nguyen <alexnguyenth96@gmail.com>
2021-12-31 13:03:49 +0700

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+const StyledH1 = styled.h1`
+  ${tw`
+    text-3xl
+    font-bold 
+    underline 
+    text-amber-400
+  `}
+`;
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-amber-400">
+    <StyledH1>
       Hello world!
-    </h1>
+    </StyledH1>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: colors
+    },
   },
   plugins: [],
 }
